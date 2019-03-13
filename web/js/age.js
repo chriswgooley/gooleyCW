dfbcc00e3e290058b304aee9111ea07f6e41120f
NerdyKittyProgrammer <33583746+NerdyKittyProgrammer@users.noreply.github.com>
2019-03-13 18:30:06 -0400

    Navbar and Portfolio Updates
    Finally (mostly) fixed the navbar! Added code samples to portfolio.

---
+++ web/js/age.js
@@ -0,0 +1,4 @@
+var userAgeYears = prompt("What is your current age in years?");
+var userAgeDays = userAgeYears * 365.25;
+
+alert("You youngun! You are approximately " + userAgeDays + " days old.");
---
+++ web/js/stalker.js
@@ -0,0 +1,9 @@
+var userNameFirst = prompt("What is your first name?");
+var userNameLast = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+var userNameFull = userNameFirst + " " + userNameLast
+
+alert("Your full name is " + userNameFull + ", and you are " + userAge + " years old.");
+
+console.log("Your full name is " + userNameFull);
+console.log("You are " + userAge + " years old");
